f0fe01477eaf207047534d271e3c71769db9a780
LauraForde <G00315765@gmit.ie>
2018-02-12 12:33:16 +0000

    Server

--- Server/connect.py
+++ Server/connect.py
@@ -1,30 +1,12 @@
 import couchdb
+
 couch = couchdb.Server('http://localhost:5984')
 
-''' Create this database
-db = couch.create('test')
-
-doc = {
-    'id':8234552,
-    'content':{
-        'person' : 'Laura'
-    }
-}
-
-db.save(doc) '''
-
 # Use existing datbase
-db = couch['test']
+db = couch.create('people')
 
-doc = {
-    'id':2458621596,
-    'content':{
-        'person' : 'Riona',
-        'email' : 'nkalndalofd'
-    }
-}
+doc = {'person': 'Riona'}
+doc1 = {'person': 'Ervin'}
 
 db.save(doc)
-
-for db in couch:
-    print(db)
+db.save(doc1)
--- Server/webapp.py
+++ Server/webapp.py
@@ -1,11 +1,29 @@
 # Adpated from http://flask.pocoo.org/
 import flask as fl
-import connect
+import couchdb
+import json
+#import connect
+from flask import Flask, render_template
+from flask import request
+
+couch = couchdb.Server('http://localhost:5984')
+db = couch['people']
 
 app = fl.Flask(__name__)
 
 @app.route("/")
+def route():
+    return render_template("home.html")
+	
+
+@app.route("/home", methods=['GET','POST'])
 def hello():
-    return "Hello World Brian!"
+
+    rows = db.view('_all_docs', include_docs = True)
+    docs = [row.doc for row in rows]
+    post = json.dumps((docs))
+    poster = json.loads(post)
+    poster.reverse()
+    return render_template("home.html", people = poster)
 
 app.run(debug = False, host="0.0.0.0")
