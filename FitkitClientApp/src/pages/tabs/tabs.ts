ddec3850db70583dc8f18b2f20ce34c268d8eb66
Riona Greally <rionagreally@gmail.com>
2018-02-27 17:00:59 +0000

    profile page/tab added

--- FitkitClientApp/src/pages/tabs/tabs.ts
+++ FitkitClientApp/src/pages/tabs/tabs.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { newTaskPage } from '../newTask/newTask';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ProfilePage } from '../profile/profile';
+
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +14,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = newTaskPage;
   tab3Root = ContactPage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
