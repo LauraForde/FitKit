b66146f8e5654268d694399e5dcaedee1ca3c073
LauraForde <G00315765@gmit.ie>
2018-03-14 18:06:16 +0000

    Removing html from server

--- Server/webapp.py
+++ Server/webapp.py
@@ -16,20 +16,17 @@ db = couch['people']
 
 @app.route('/')
 def route():
-    return render_template('home.html')
+    return "Welcome to FitKit"
 	
 
+# With help from - http://flask.pocoo.org/docs/0.12/api/
 @app.route("/people", methods = ['GET'])
 def hello():
     docs = []
     
     rows = db.view('_all_docs', include_docs = True)
     docs = [row.doc for row in rows]
-    # post = json.dumps((docs))
-    # poster = json.loads(post)
-    # poster.reverse()
-    
-
     return json.dumps(docs)
+
 if __name__ == "__main__":
     app.run(debug = False, host="0.0.0.0")
