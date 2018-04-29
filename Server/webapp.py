82c89b63fb4aed4266b6e7d400051d46f7d3a30d
LauraForde <G00315765@gmit.ie>
2018-04-29 22:38:20 +0000

    Removing Pem file

--- Server/webapp.py
+++ Server/webapp.py
@@ -2,7 +2,6 @@
 import flask as fl
 import couchdb
 import json
-#import connect
 from flask import Flask, render_template
 from flask import request, json, jsonify
 from flask_cors import CORS, cross_origin
@@ -18,7 +17,52 @@ gymEquip = couch['equipment']
 @app.route('/')
 def route():
     return "Welcome to FitKit"
-	
+
+@app.route("/login", methods = ['GET'])
+def login():
+
+    username = request.json["username"]
+    password = request.json["password"]
+
+    if username & password in db:
+        try:
+            couch.resource.credentials=(username, password)
+        except: 
+            print ("Unexpected error")
+    else:
+        print("Username or Password wrong")
+        
+@app.route("/reg", methods= ['GET', 'POST'])
+def reg_person() :
+
+    try:
+        firstname = request.json["firstname"]
+        surname = request.json["surname"]
+        email = request.json["email"]
+        username = request.json["username"]
+        password = request.json["password"]
+        gender = request.json["gender"]
+        dob = request.json["dob"]
+        bio = request.json["bio"]
+
+        print (type(request.json))
+
+        doc = {
+            'firstname': firstname,
+            'surname': surname,
+            'email': email,
+            'username': username,
+            'password': password,
+            'gender': gender,
+            'dob': dob,
+            'bio': bio
+        }
+        db.save(doc)
+    except: 
+        print ("Unexpected error")
+    
+
+    return json.dumps({"message": "succesfull"}),200, {'Content-Type', 'application/json'}
 
 # With help from - http://flask.pocoo.org/docs/0.12/api/
 @app.route("/people", methods = ['GET'])
@@ -28,6 +72,19 @@ def people():
     rows = db.view('_all_docs', include_docs = True)
     docs = [row.doc for row in rows]
     return json.dumps(docs)
+
+@app.route("/shop", methods = ['GET'])
+def shop():
+    docsEquip = []
+    
+    rows = gymEquip.view('_all_docs', include_docs = True)
+    docsEquip = [row.doc for row in rows]
+    return json.dumps(docsEquip)
+
+@app.route("/deletePost", methods = ['GET', 'POST'])
+def delete_post(self, id):
+    to_delete = gymEquip[id]
+    gymEquip.delete(to_delete)
 
 @app.route("/update", methods = ['GET', 'POST'])
 def update(self, id, json):
@@ -45,27 +102,6 @@ def delete_person():
         db.delete(to_delete)
     db.update(docs)
     return docs
-
-@app.route("/reg", methods= ['GET', 'POST'])
-def reg_person() :
-    user = request.json
-    json.dumps(dict(user))
-    db.save(user)
-
-    return {"message": "succesfull"},200
-
-@app.route("/shop", methods = ['GET'])
-def shop():
-    docsEquip = []
-    
-    rows = gymEquip.view('_all_docs', include_docs = True)
-    docsEquip = [row.doc for row in rows]
-    return json.dumps(docsEquip)
-
-@app.route("/deletePost", methods = ['GET', 'POST'])
-def delete_post(self, id):
-    to_delete = gymEquip[id]
-    gymEquip.delete(to_delete)
 
 if __name__ == "__main__":
     app.run(debug = False, host="0.0.0.0")
