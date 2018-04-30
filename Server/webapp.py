1ffd741ef50b9adf2aecdd65a56ad4f78ffe8c7b
LauraForde <G00315765@gmit.ie>
2018-04-30 01:07:28 +0000

    db things

--- Server/webapp.py
+++ Server/webapp.py
@@ -10,28 +10,31 @@ app = fl.Flask(__name__)
 
 cors = CORS(app, resources={r"/*": {"origins":"*"}})
 
+# Connecting to couch server
 couch = couchdb.Server('http://localhost:5984')
+
+# Connecting to two different databases
 db = couch['people']
 gymEquip = couch['equipment']
 
+# Simple route
 @app.route('/')
 def route():
     return "Welcome to FitKit"
 
+# Login route
 @app.route("/login", methods = ['GET'])
 def login():
 
     username = request.json["username"]
     password = request.json["password"]
-
-    if username & password in db:
-        try:
-            couch.resource.credentials=(username, password)
-        except: 
-            print ("Unexpected error")
+    print(type(request.json))
+    if username in db:
+        print ("USERNAME EXISTS")
     else:
-        print("Username or Password wrong")
-        
+        print("NO")
+
+# Register new user  
 @app.route("/reg", methods= ['GET', 'POST'])
 def reg_person() :
 
@@ -65,38 +68,72 @@ def reg_person() :
     return json.dumps({"message": "succesfull"}),200, {'Content-Type', 'application/json'}
 
 # With help from - http://flask.pocoo.org/docs/0.12/api/
+# Showing all people
 @app.route("/people", methods = ['GET'])
 def people():
     docs = []
-    
     rows = db.view('_all_docs', include_docs = True)
     docs = [row.doc for row in rows]
     return json.dumps(docs)
 
+# Showing items in equipment database
 @app.route("/shop", methods = ['GET'])
 def shop():
     docsEquip = []
-    
     rows = gymEquip.view('_all_docs', include_docs = True)
     docsEquip = [row.doc for row in rows]
     return json.dumps(docsEquip)
 
-@app.route("/deletePost", methods = ['GET', 'POST'])
-def delete_post(self, id):
-    to_delete = gymEquip[id]
-    gymEquip.delete(to_delete)
+# Route for adding to the equipment db
+@app.route("/newPost", methods= ['GET', 'POST'])
+def new_post():
+    
+    try:
+        shopID = request.json["shopID"]
+        prodName = request.json["prodName"]
+        price = request.json["price"]
+        des = request.json["des"]
+        sellName = request.json["sellName"]
+        num = request.json["num"]
 
+        print (type(request.json))
+
+        doc = {
+            'shopID': shopID,
+            'prodName': prodName,
+            'price': price,
+            'des': des,
+            'sellName': sellName,
+            'num': num
+        }
+        gymEquip.save(doc)
+    except: 
+        print ("Unexpected error")
+
+        
+    return json.dumps({"message": "successfull"}),200, {'Content-Type', 'application/json'}
+
+
+# Attempt at deleting post
+@app.route("/deletePost", methods = ['GET', 'POST'])
+def delete_post():
+    shopID = request.json["shopID"]
+
+    gymEquip.delete(shopID)
+    return json.dumps({"message": "successful"}), 200, {'Content-Type', 'application/json'}
+
+# Attempt at updating
 @app.route("/update", methods = ['GET', 'POST'])
 def update(self, id, json):
     doc = self.db.get(id)
     doc["name"] = json["name"]
     doc = self.db.save(doc)
 
+# Attempt at deleting person
 @app.route("/deletePerson", methods = ['GET', 'POST', 'DELETE'])
 def delete_person():
 
-    id = request.form['id']
-
+    id = request.json['id']
     if id in db:
         to_delete = db[id]
         db.delete(to_delete)
